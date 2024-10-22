f4d86e79d0abd6af6b1d1e225dad28ea4d8c1b19
Hex-Umbra <g.p.dacosta@hotmail.com>
2024-10-22 19:55:07 +0200

    Restructured the whole code for more readability, added session and flash messages for server data store for temporary data, and feedback for when something happens in the app.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,6 +6,14 @@ const bcrypt = require("bcryptjs");
 const token = require("jsonwebtoken");
 
 //Routes
+
+//Get route to get all users
+router.get("/admin/users",async(req,res) =>{
+  try {const authHeader
+})
+
+//Registering route for a new user, by checking the e-mail address we verify the user doesn't exist in our DB and add it as a new user.
+//The password is encrypted by bcrypt with genSalt & hashSync for user security.
 router.post("/register", async (req, res) => {
   try {
     const newUser = req.body;
@@ -32,16 +40,20 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//Logging-in Route, if the the user exists in the DB they can login using the e-mail address and their corresponding password.
 router.post("/login", async (req, res) => {
   const { email, password } = req.body;
   const user = await userModel.findOne({ email });
   if (user) {
+    //After logging in we decrypt the password and compare it with the one the user entered.
     const isMatch = await bcrypt.compare(password, user.password);
     if (isMatch) {
+      //If the password is correct we generate a token and send it back to the user.
       const cookieToken = token.sign(
         { role: user.role },
         process.env.JWT_SECRET
       );
+      //We send the token into an HTTP cookie, with some security mesures.
       res
         .cookie("token", cookieToken, {
           httpOnly: true,
@@ -53,6 +65,10 @@ router.post("/login", async (req, res) => {
     } else res.json({ message: "Email or password incorrect" });
   } else res.status(400).json({ message: "This user does not exist" });
 });
+
+//Delete route for the user or admin.  This route is protected by a token.
+//The user can delete its own profile but not other users profile
+//While the admin can delete any user profile
 router.delete("/user/:id", verifyToken, async (req, res) => {
   try {
     const userId = req.params.id;
@@ -60,17 +76,20 @@ router.delete("/user/:id", verifyToken, async (req, res) => {
     if (!user) {
       res.status(404).json({ message: "User  not found" });
     } else {
-      if(decoded.role === "admin" || decoded.role){
+      if (decoded.role === "admin" || decoded.role) {
         await userModel.findByIdAndDelete(userId);
-        res.status(200)
-      } 
+        res.status(200);
+      }
     }
   } catch (error) {
-    res.status(500).json({ message: `Oops Looks like something went wrong :${error}` });
+    res
+      .status(500)
+      .json({ message: `Oops Looks like something went wrong :${error}` });
   }
 });
 
 //Functions
+//Function to verify who has authorisation by checking the token.
 function verifyToken(req, res, next) {
   const userToken = req.cookies.token;
   if (!userToken) {
@@ -83,7 +102,7 @@ function verifyToken(req, res, next) {
       return res.status(401).json({ message: "Unauthorized" });
     }
     req.user = decoded;
-    next()
+    next();
   });
 }
 
